bb76977fd1254d276efeffe092f71d3ef60f190e
teodoro2004 <tteodoro080@gmail.com>
2024-06-12 12:28:33 -0300

    Delete Basic/solved directory

--- Basic/solved/exercicio12.js
+++
@@ -1,9 +0,0 @@
-function somaDeUmAteDez() {
-    parametro = 10;
-    let soma = 0;
-    for (let numero = 1; numero <= parametro; numero++) {
-        console.log(soma += numero);
-
-    }
-  }
-  console.log(somaDeUmAteDez());  
--- Basic/solved/exercicio5.js
+++
@@ -1,9 +0,0 @@
-let numeros = [1, 2, 3, 4, 5];
-
-function mostrarNumeros(numeros) {
-    let primeiro = numeros[0];
-    let segundo = numeros[4];
-    console.log(primeiro + ' and ' + segundo);
-}
-
-mostrarNumeros(numeros);
